66dc68acdcbcb6811d52e063dfdca3d53562a282
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-03-26 12:16:51 +0100

    Switch from Mocha to ava for tests
    Ava gives runs tasks in parallel and has native promise/async support

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,8 @@
-import {expect} from 'chai';
+import test from 'ava';
 import {add} from '../src/add.js';
 
-describe('SampleTest', () => {
-    it('should add numbers', () => {
-        expect(add(2, 2)).to.equal(4);
-    });
+test('Add() should add numbers', t => {
+    t.is(add(2, 2), 4);
+    t.is(add(0, 2), 2);
+    t.is(add(0, -2), -2);
 });
