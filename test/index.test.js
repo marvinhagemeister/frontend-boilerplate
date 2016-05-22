419479dd153a38bc9de404b247352555c909d3cb
Marvin Hagemeister <marvin@marvinhagemeister.de>
2016-05-22 17:21:24 +0200

    Switch back to mocha as test runner because of karma

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,10 @@
-import test from 'ava';
+import {assert as t} from 'chai';
 import {add} from '../src/add.js';
 
-test('Add() should add numbers', t => {
-    t.is(add(2, 2), 4);
-    t.is(add(0, 2), 2);
-    t.is(add(0, -2), -2);
+describe('Add', () => {
+    it('should add numbers', () => {
+        t.equal(add(2, 2), 4);
+        t.equal(add(0, 2), 2);
+        t.equal(add(0, -2), -2);
+    });
 });
